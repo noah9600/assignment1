c880486b2c7837317f1452d15af2071e0b2cfe2c
Mao Tabuchi <zzzmao0423@gmail.com>
2023-09-28 09:22:34 -0400

    chanvge

---
+++ public/Scripts/index.js
@@ -0,0 +1,9 @@
+function showAlert() {
+    alert("Hello, this is an alert!");
+}
+
+// Function to change the text of an element when a button is clicked
+function changeText() {
+    var paragraph = document.getElementById("demoParagraph");
+    paragraph.innerHTML = "Text has been changed!";
+}
